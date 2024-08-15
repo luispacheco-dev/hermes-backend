06e3e3a2dd91d51fec817a569bb13ff6129fbe7c
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-15 16:42:00 -0700

    Get Friend Code Implemented

--- xprofile/utils.py
+++ xprofile/utils.py
@@ -1,2 +1,14 @@
+from random import choices
+from string import digits, ascii_uppercase
+
 def get_upload_path(instance, filename):
     return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+def get_friend_code(cls):
+    friend_code = get_random_string()
+    while len(cls.objects.filter(code=friend_code)) != 0:
+        friend_code = get_random_string()
+    return friend_code
+
+def get_random_string():
+    return ''.join(choices(ascii_uppercase+digits, k=8))
