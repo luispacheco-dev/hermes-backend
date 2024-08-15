1f07e9add4ca46fee8ffac2ae8d69c6f16e8cd59
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-15 13:08:22 -0700

    Custom Auth User Model Added

--- xcore/settings.py
+++ xcore/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# AUTHOR'S CUSTOM SETTINGS
+
+AUTH_USER_MODEL = 'xprofile.CustomUser'
