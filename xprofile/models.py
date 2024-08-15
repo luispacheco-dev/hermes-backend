f4dfaffc87e2a0ce6d064f55b91180c5a4096ece
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-15 13:06:40 -0700

    Custom User Model

--- xprofile/models.py
+++ xprofile/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from .managers import CustomUserManager
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
