c0f488c8fd2a3bcea942b4732785c902bd3d3733
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-15 14:18:24 -0700

    Media Settings (Url and Root) Added

--- xcore/settings.py
+++ xcore/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,3 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # AUTHOR'S CUSTOM SETTINGS
 
 AUTH_USER_MODEL = 'xprofile.CustomUser'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
