67a3d9f16bd81021305350a28424e334d31dc1ec
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-16 15:53:32 -0700

    Create Friend Record View

--- xfriend/urls.py
+++ xfriend/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from .views import CreateFriendView
 from .views import CreateFriendRequestView
 
 
 app_name = 'xfriend'
 
 urlpatterns = [
+    path('', CreateFriendView.as_view(), name='create-friend'),
     path('friend-requests/', CreateFriendRequestView.as_view(), name='friend-requests'),
 ]
