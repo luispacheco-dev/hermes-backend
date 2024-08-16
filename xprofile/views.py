774e9878aeb7ac6a2f1519381b8d80f051c9402b
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-15 18:18:30 -0700

    Update Logged State Implemented

--- xprofile/views.py
+++ xprofile/views.py
@@ -63,3 +63,20 @@ class ProfileByIdView(views.APIView):
         profile.picture = profile.picture if 'picture' not in payload else payload['picture']
         profile.save()
         return Response(data=ProfileModelSerializer(profile).data, status=200)
+
+
+class ProfileByIdLoggedView(views.APIView):
+
+    def patch(self, request, id):
+        try:
+            profile = Profile.objects.get(id=id)
+        except:
+            return Response(data={'error(s)': "Profile Doesn't Exist"}, status=400)
+        if request.user != profile.user:
+            return Response(data={'error(s)': 'Forbidden Resource'}, status=403)
+        payload = request.data
+        if 'logged' not in payload or type(payload['logged']) is not bool:
+            return Response(data={'error(s)': 'Logged Field Is Required (Bool)'}, status=400)
+        profile.logged = payload['logged']
+        profile.save()
+        return Response(data={'logged': payload['logged']}, status=200)
