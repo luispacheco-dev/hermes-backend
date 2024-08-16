5e7e8cd064f9c7d36c09aa6ca1813e7dd575a440
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-16 14:11:07 -0700

    Get Profile By Id Friend Request View

--- xprofile/urls.py
+++ xprofile/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import ProfileByIdView
 from .views import CreateUserProfileView
 from .views import ProfileByIdLoggedView
+from .views import GetProfileFriendRequestView
 
 
 app_name = 'xprofile'
@@ -10,4 +11,5 @@ urlpatterns = [
     path('<int:id>/', ProfileByIdView.as_view(), name='profile-by-id'),
     path('', CreateUserProfileView.as_view(), name='create-user-profile'),
     path('<int:id>/logged/', ProfileByIdLoggedView.as_view(), name='profile-by-id-logged'),
+    path('<int:id>/friend-requests/', GetProfileFriendRequestView.as_view(), name='profile-by-id-friend-requests'),
 ]
