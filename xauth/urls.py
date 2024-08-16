ad988e088377adf72736a098fb21f42aa1064444
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-15 17:26:56 -0700

    Token Verify View

--- xauth/urls.py
+++ xauth/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenVerifyView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 
 app_name = 'xauth'
 
 urlpatterns = [
     path('token/', CustomTokenObtainPairView.as_view(), name='token'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token-verify'),
 ]
