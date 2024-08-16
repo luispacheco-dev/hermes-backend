8a874ee00f254e2c3ef70c5e84e59d7d9d85a51f
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-16 16:34:58 -0700

    Delete Friend View

--- xfriend/urls.py
+++ xfriend/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import CreateFriendView
+from .views import DeleteFriendView
 from .views import CreateFriendRequestView
 
 
@@ -7,5 +8,6 @@ app_name = 'xfriend'
 
 urlpatterns = [
     path('', CreateFriendView.as_view(), name='create-friend'),
+    path('<int:id>/', DeleteFriendView.as_view(), name='delete-friend'),
     path('friend-requests/', CreateFriendRequestView.as_view(), name='friend-requests'),
 ]
