f2fef46b9be715bd094b2e248227928b940656a5
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-16 15:53:18 -0700

    Create Friend Record Implemented

--- xfriend/views.py
+++ xfriend/views.py
@@ -1,7 +1,9 @@
+from .models import Friend
 from rest_framework import views
 from .models import FriendRequest
 from xprofile.models import Profile
 from rest_framework.response import Response
+from .serializers import FriendModelSerializer
 from .serializers import FriendRequestModelSerializer
 
 
@@ -33,3 +35,32 @@ class CreateFriendRequestView(views.APIView):
             print(e)
             return Response(data={'error(s)': 'Internal Error'}, status=500)
         return Response(data=FriendRequestModelSerializer(friend_request).data, status=201)
+
+
+class CreateFriendView(views.APIView):
+
+    def post(self, request):
+        payload = request.data
+        friend_serializer = FriendModelSerializer(data=payload)
+        if not friend_serializer.is_valid():
+            return Response(data=friend_serializer.errors, status=400)
+        try:
+            receiver_profile = Profile.objects.get(id=payload['receiver'])
+        except Profile.DoesNotExist:
+            return Response(data={'error(s)': "Receiver Profile Doesn't Exist"}, status=400)
+        if request.user != receiver_profile.user:
+            return Response(data={'error(s)': 'Forbidden Resource'}, status=403)
+        try:
+            sender_profile = Profile.objects.get(id=payload['sender'])
+        except Profile.DoesNotExist:
+            return Response(data={'error(s)': "Sender Profile Doesn't Exist"}, status=400)
+        if len(FriendRequest.objects.filter(sender=sender_profile, code=receiver_profile.code)) == 0:
+            return Response(data={'error(s)': "Friend Request Doesn't Exist"}, status=400)
+        if len(Friend.objects.filter(sender=sender_profile, receiver=receiver_profile)) != 0:
+            return Response(data={'error(s)': 'Friend Already Exist'}, status=400)
+        try:
+            friend = Friend.objects.create(sender=sender_profile, receiver=receiver_profile)
+        except Exception as e:
+            print(e)
+            return Response(data={'error(s)': 'Internal Error'}, status=500)
+        return Response(data=FriendModelSerializer(friend).data, status=201)
