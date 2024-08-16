9566726f6917d655a41c4e9ea601a5aad66b1591
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-15 17:36:54 -0700

    Profile By Id View

--- xprofile/urls.py
+++ xprofile/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from .views import ProfileByIdView
 from .views import CreateUserProfileView
 
 
 app_name = 'xprofile'
 
 urlpatterns = [
+    path('<int:id>/', ProfileByIdView.as_view(), name='profile-by-id'),
     path('', CreateUserProfileView.as_view(), name='create-user-profile'),
 ]
