b1ba10a82a91693fdb7eaddca370c34e885b724f
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-16 15:18:40 -0700

    Delete Friend Request View

--- xprofile/urls.py
+++ xprofile/urls.py
@@ -3,6 +3,7 @@ from .views import ProfileByIdView
 from .views import CreateUserProfileView
 from .views import ProfileByIdLoggedView
 from .views import GetProfileFriendRequestView
+from .views import DeleteProfileFriendRequestView
 
 
 app_name = 'xprofile'
@@ -12,4 +13,5 @@ urlpatterns = [
     path('', CreateUserProfileView.as_view(), name='create-user-profile'),
     path('<int:id>/logged/', ProfileByIdLoggedView.as_view(), name='profile-by-id-logged'),
     path('<int:id>/friend-requests/', GetProfileFriendRequestView.as_view(), name='profile-by-id-friend-requests'),
+    path('<int:p_id>/friend-requests/<int:r_id>/', DeleteProfileFriendRequestView.as_view(), name='profile-by-id-friend-requests-delete'),
 ]
