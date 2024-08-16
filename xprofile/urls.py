02ce90400d91883989fb6c186b9d6414e00803c2
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-15 18:18:51 -0700

    Profile By Id Logged View

--- xprofile/urls.py
+++ xprofile/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import ProfileByIdView
 from .views import CreateUserProfileView
+from .views import ProfileByIdLoggedView
 
 
 app_name = 'xprofile'
@@ -8,4 +9,5 @@ app_name = 'xprofile'
 urlpatterns = [
     path('<int:id>/', ProfileByIdView.as_view(), name='profile-by-id'),
     path('', CreateUserProfileView.as_view(), name='create-user-profile'),
+    path('<int:id>/logged/', ProfileByIdLoggedView.as_view(), name='profile-by-id-logged'),
 ]
