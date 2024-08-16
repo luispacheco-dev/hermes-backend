eea0f3f3c3ca285ff5ea4d8b9fa8a7e6ebad647c
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-16 13:06:36 -0700

    Friend and FriendRequest models

--- xfriend/models.py
+++ xfriend/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from xprofile.models import Profile
 
-# Create your models here.
+
+class Friend(models.Model):
+    began_at = models.DateTimeField(auto_now_add=True)
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
+
+    def __str__(self):
+        return self.sender.user.email + ' + ' + self.receiver.user.email
+
+
+class FriendRequest(models.Model):
+    code = models.CharField(max_length=8)
+    greetings = models.CharField(max_length=254)
+    requested_at = models.DateTimeField(auto_now_add=True)
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.sender.user.email + ' + ' + self.code
