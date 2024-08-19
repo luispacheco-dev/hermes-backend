5ef903b4eeca5eba5b82250fe384e55e7cd33c0b
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-19 13:28:41 -0700

    Get Messages Implemented

--- xchat/views.py
+++ xchat/views.py
@@ -31,7 +31,7 @@ class CreateChatView(views.APIView):
         return Response(data=ChatModelSerializer(chat).data, status=201)
 
 
-class CreateMessageView(views.APIView):
+class MessagesView(views.APIView):
 
     def post(self, request, id):
         try:
@@ -57,3 +57,14 @@ class CreateMessageView(views.APIView):
             print(e)
             return Response(data={'error(s)': 'Internal Error'}, status=500)
         return Response(data=MessageModelSerializer(message).data, status=201)
+
+    def get(self, request, id):
+        try:
+            chat = Chat.objects.get(id=id)
+        except Chat.DoesNotExist:
+            return Response(data={'error(s)': "Chat Doesn't Exist"}, status=400)
+        if request.user != chat.friend.sender.user and request.user != chat.friend.receiver.user:
+            return Response(data={'error(s)': 'Forbidden Resource'}, status=403)
+        messages = Message.objects.filter(chat=chat)
+        messages = [MessageModelSerializer(message).data for message in messages]
+        return Response(data=messages, status=200)
