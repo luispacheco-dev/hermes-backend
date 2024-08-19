06a9cfee7e25d528a9a2f4247e4d36ca76fd6767
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-19 11:47:57 -0700

    Chat and Message Models

--- xchat/models.py
+++ xchat/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from xfriend.models import Friend
+from xprofile.models import Profile
 
-# Create your models here.
+
+class Chat(models.Model):
+    friend = models.ForeignKey(Friend, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Chat " + self.friend
+
+
+class Message(models.Model):
+    content = models.CharField(max_length=254)
+    readed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
