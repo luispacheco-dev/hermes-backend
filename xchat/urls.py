e76655f5d887fa84ca89f1c220bddd8697b6403a
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-19 14:24:35 -0700

    Update Messages Readed State View

--- xchat/urls.py
+++ xchat/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import MessagesView
 from .views import CreateChatView
+from .views import ReadMessagesView
 
 
 app_name = 'xchat'
 
 urlpatterns = [
     path('', CreateChatView.as_view(), name='create-chat'),
+    path('<int:id>/messages/readed/', ReadMessagesView.as_view(), name='readed'),
     path('<int:id>/messages/', MessagesView.as_view(), name='get-create-messages'),
 ]
