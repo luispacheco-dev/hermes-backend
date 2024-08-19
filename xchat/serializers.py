8a2bf6a7937fb622ee614d4305bf9d078ac9f30a
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-19 11:53:49 -0700

    Chat and Message Models Serializer

---
+++ xchat/serializers.py
@@ -0,0 +1,18 @@
+from .models import Chat
+from .models import Message
+from rest_framework import serializers
+
+
+class ChatModelSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Chat
+        fields = '__all__'
+
+
+class MessageModelSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Message
+        fields = '__all__'
+        read_only_fields = ['readed', 'created_at']
