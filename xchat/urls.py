65eb0b7cafeb8ef9a8cf048a1e3b71d57028f380
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-19 13:21:14 -0700

    Create Message View

--- xchat/urls.py
+++ xchat/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import CreateChatView
+from .views import CreateMessageView
 
 
 app_name = 'xchat'
 
 urlpatterns = [
     path('', CreateChatView.as_view(), name='create-chat'),
+    path('<int:id>/messages/', CreateMessageView.as_view(), name='create-message'),
 ]
